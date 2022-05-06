525764f240e3f80efa15d6973ba494dddf6e027d
Ose Ughu <zinjmyster10@gmail.com>
2022-05-06 11:35:00 +0100

    add redis caching

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,27 +2,56 @@ import mongoose from 'mongoose'
 import Redis from 'ioredis'
 
 export const connectToDb = () => mongoose.connect(process.env.MONGO_URL)
+const redis = new Redis(process.env.REDIS_URL)
+
+async function clearCachedData(collectionName, op) {
+  const allowedCacheOps = ['find', 'findById', 'findOne']
+  // if operation is insert or delete or update for any collection that exists and has cached values
+  // delete its childern
+  if (!allowedCacheOps.includes(op) && (await redis.exists(collectionName))) {
+    redis.del(collectionName)
+  }
+}
 
 export const applyMongooseCache = () => {
-  const redis = new Redis(process.env.REDIS_URL)
   const exec = mongoose.Query.prototype.exec
 
   mongoose.Query.prototype.exec = async function () {
-    const key = JSON.stringify({
-      ...this.getQuery(),
-      collection: this.mongooseCollection.name,
-      op: this.op,
-      options: this.options
-    })
+    const collectionName = this.mongooseCollection.name
 
-    const cacheValue = await redis.get(key)
+    if (this.cacheMe) {
+      // You can't insert json straight to redis needs to be a string
 
-    cacheValue && JSON.parse(cacheValue)
+      const key = JSON.stringify({
+        ...this.getOptions(),
+        collectionName: collectionName,
+        op: this.op
+      })
+      const cachedResults = await redis.hget(collectionName, key)
 
-    const result = await exec.apply(this, arguments)
+      // getOptions() returns the query and this.op is the method which in our case is "find"
 
-    result && redis.set(key, JSON.stringify(result))
+      if (cachedResults) {
+        // if you found cached results return it;
+        const result = JSON.parse(cachedResults)
+        return result
+      }
+      //else
+      // get results from Database then cache it
+      const result = await exec.apply(this, arguments)
 
-    return result
+      redis.hset(
+        collectionName,
+        key,
+        JSON.stringify(result),
+        'EX',
+        this.cacheTime
+      )
+
+      return result
+    }
+
+    clearCachedData(collectionName, this.op)
+    return exec.apply(this, arguments)
   }
 }
